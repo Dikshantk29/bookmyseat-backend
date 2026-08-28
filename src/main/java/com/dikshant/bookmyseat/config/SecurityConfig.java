4ced5a6b50b3996f515a2909beec6697eb40d5e6
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2026-08-28 12:49:56 +0530

    feat: register JWT authentication filter

--- src/main/java/com/dikshant/bookmyseat/config/SecurityConfig.java
+++ src/main/java/com/dikshant/bookmyseat/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.dikshant.bookmyseat.config;
 
+import com.dikshant.bookmyseat.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,9 +10,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,6 +32,10 @@ public class SecurityConfig {
                                 "/api/users/login"
                         ).permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
